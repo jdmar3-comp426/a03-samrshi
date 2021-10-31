b9a8a85bc68a6f22efd21c31d2ea156664b1311f
samrshi <samrshi@outlook.com>
2021-10-31 17:27:35 -0400

    getMedian

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -35,8 +35,6 @@ export function getMedian(array) {
     }
 }
 
-console.log(getMedian([14,8,5,7,7,5,2,7,5,3,47,9]));
-
 /**
  * Calculates the mean of an array of numbers.
  * @param {number[]} array
